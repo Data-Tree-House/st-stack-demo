3fd79a507ccb3fafa33dc8e6f61289efac8ee629
Johandielangman <jghanekom2@gmail.com>
2026-05-31 16:34:41 +0200

    fix: pytests

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,15 +3,10 @@ from collections.abc import Generator
 from pathlib import Path
 
 import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.engine import Engine
-from testcontainers.postgres import PostgresContainer
-
-from db import crud
 
 
 @pytest.fixture
-def temp_dir() -> Generator[Path, None, None]:
+def temp_dir() -> Generator[Path]:
     """Create a temporary directory that's cleaned up after the test."""
     with tempfile.TemporaryDirectory(
         dir=Path(__file__).parent / "tmp",
@@ -19,25 +14,25 @@ def temp_dir() -> Generator[Path, None, None]:
         yield Path(tmpdir)
 
 
-@pytest.fixture(scope="class")
-def mock_engine() -> Generator[Engine, None, None]:
-    """Create a fresh in-memory SQLite database for each test."""
-    engine = create_engine("sqlite:///:memory:", echo=True)
-    crud.create_all_tables(engine)
+# @pytest.fixture(scope="class")
+# def mock_engine() -> Generator[Engine]:
+#     """Create a fresh in-memory SQLite database for each test."""
+#     engine = create_engine("sqlite:///:memory:", echo=True)
+#     crud.create_all_tables(engine)
 
-    yield engine
+#     yield engine
 
-    # However, there are many cases where it is desirable that all connection resources referred to by the Engine be
-    # completely closed out. It's generally not a good idea to rely on Python garbage collection for this to occur for
-    # these cases; instead, the Engine can be explicitly disposed using the Engine.dispose() method.
-    # ref: https://docs.sqlalchemy.org/en/21/core/connections.html
-    engine.dispose()
+#     # However, there are many cases where it is desirable that all connection resources referred to by the Engine be
+#     # completely closed out. It's generally not a good idea to rely on Python garbage collection for this to occur for
+#     # these cases; instead, the Engine can be explicitly disposed using the Engine.dispose() method.
+#     # ref: https://docs.sqlalchemy.org/en/21/core/connections.html
+#     engine.dispose()
 
 
-@pytest.fixture(scope="module")
-def pg_engine() -> Generator[Engine, None, None]:
-    with PostgresContainer("postgres:18-alpine") as postgres:
-        engine = create_engine(postgres.get_connection_url(), echo=True)
-        crud.create_all_tables(engine)
-        yield engine
-        engine.dispose()
+# @pytest.fixture(scope="module")
+# def pg_engine() -> Generator[Engine]:
+#     with PostgresContainer("postgres:18-alpine") as postgres:
+#         engine = create_engine(postgres.get_connection_url(), echo=True)
+#         crud.create_all_tables(engine)
+#         yield engine
+#         engine.dispose()
--- src/tests/test_hello.py
+++ src/tests/test_hello.py
@@ -1,2 +1,14 @@
+import pytest
+
+from core.utils.palette import google_text
+
+
 def test_hello():
     assert "hello" == "hello"
+
+
+def test_google_text():
+    try:
+        google_text()
+    except Exception as e:
+        pytest.fail(f"{e}")
